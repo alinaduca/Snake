95f5f05112fd484b5fbeb52d4d2c3b36922dfdd8
Alina Duca <alina_duca@yahoo.com>
2023-12-29 10:30:15 +0200

    Adding images for obstacle

--- Obstacle.py
+++ Obstacle.py
@@ -6,7 +6,11 @@ obstacle_color = (56, 74, 12)
 class Obstacle:
     def __init__(self):
         self.pos = Vector2(5, 10)
+        self.obstacle = pygame.image.load('Images/cactus (2).png').convert_alpha()
 
     def draw(self, size, surface):
+        # obstacle_rectangle = pygame.Rect(int(self.pos.x * size), int(self.pos.y * size), size, size)
+        # pygame.draw.rect(surface, obstacle_color, obstacle_rectangle)
+
         obstacle_rectangle = pygame.Rect(int(self.pos.x * size), int(self.pos.y * size), size, size)
-        pygame.draw.rect(surface, obstacle_color, obstacle_rectangle)
+        surface.blit(self.obstacle, obstacle_rectangle)
